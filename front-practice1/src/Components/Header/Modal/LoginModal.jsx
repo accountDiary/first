62c795458e2c89fa95ed414c394515de7a1b8779
jyekim <jye7921@gmail.com>
2024-06-28 21:45:15 +0900

    .
    .

--- front-practice1/src/Components/Header/Modal/LoginModal.jsx
+++ front-practice1/src/Components/Header/Modal/LoginModal.jsx
@@ -1,29 +1,27 @@
 import React from "react";
 import Modal from "react-modal";
 import "../../../Css/Modal.css";
-import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const LoginModal = ({ onClose }) => {
 
-  // const moveCreateAccount = useNavigate();
-
-  // function createAccountPage() {
-  //   moveCreateAccount("/createAccount")  
-  // }
-  return (
-    <Modal
-      isOpen={true}
-      onRequestClose={onClose}
-      className="ReactModal__Content"
-      overlayClassName="ReactModal__Overlay"
-    >
-      <h2>로그인</h2>
-      <input type="text" placeholder="아이디" />
-      <input type="password" placeholder="비밀번호" />
-      <button onClick={onClose}>닫기</button>
-      {/* <button onClick={createAccountPage}>회원가입</button> */}
-    </Modal>
-  );
+    const createAccountPage = () => {
+        onClose(); // 모달 닫기
+    };
+    return (
+        <Modal
+            isOpen={true}
+            onRequestClose={onClose}
+            className="ReactModal__Content"
+            overlayClassName="ReactModal__Overlay"
+        >
+            <h2>로그인</h2>
+            <input type="text" placeholder="아이디" />
+            <input type="password" placeholder="비밀번호" />
+            <button onClick={onClose}>닫기</button>
+            <Link to="/createAccount" onClick={createAccountPage}>회원가입</Link>
+        </Modal>
+    );
 };
 
 export default LoginModal;
