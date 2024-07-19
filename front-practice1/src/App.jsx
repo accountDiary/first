303806f260971580d6de1c2ae1d7f2e1608ea361
jyekim <jye7921@gmail.com>
2024-07-19 23:59:16 +0900

    Update App.jsx

--- front-practice1/src/App.jsx
+++ front-practice1/src/App.jsx
@@ -5,6 +5,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Header from "./Components/Header/Header.jsx";
 import Footer from "./Components/Footer/Footer.jsx";
 import CreateAccount from "./Components/CreateAccount/CreateAccount.jsx";
+<<<<<<< Updated upstream
 import { Provider } from 'react-redux';
 import store from './Redux/store.js';
 
@@ -27,6 +28,24 @@ function App() {
         </BrowserRouter>
         </Provider>
 
+=======
+
+function App() {
+    return (
+        <BrowserRouter>
+            <div className="App">
+                <Header />
+
+                <Routes>
+                    <Route path="/createAccount" element={<CreateAccount />} />
+
+                    <Route path="/" element={<FullCalendar1 />} />
+
+                </Routes>
+                <Footer />
+            </div>
+        </BrowserRouter>
+>>>>>>> Stashed changes
     );
 }
 
